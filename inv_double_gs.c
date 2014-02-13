4f6281734fde1c000d3da6c63df524cac3f7802d
Will Childs-Klein <willck93@gmail.com>
2014-02-12 22:04:24 -0500

    single gs inversion working

--- inv_double_gs.c
+++ inv_double_gs.c
@@ -8,11 +8,10 @@ void inv_double_gs(double* a, int n, double* u, double* b);
 
 // VECTOR ROUTINES: user must allocate memory for return param w
 void vectorSubtract(int n, double* u, double* v, double* w); 	// u - v = w
-void vectorAdd(int n, double* u, double* v, double* w); 		// u + v = w
 void vectorScale(int n, double* u, double c, double* w);		// cu = w
 void dotProduct(int n, double* u, double* v, double* w);		// u * v = w
-void vectorProject(int n, double* u, double* v, double* w);		// comp(u,v) = w
-void vectorNormalize(int n, double* u, double* w);				// ||u|| = w
+void vectorProject(int n, double* u, double* v, double* x, double* w);// ((u*v) / ||v||^2)x = w
+void vectorNormalize(int n, double* u, double* v, double* w);	// u / ||v|| = w
 void vectorMagnitude(int n, double* u, double* w);				// |u| = w
 
 // MATRIX ROUTINES: user must allocate memory for retrun param w
@@ -30,35 +29,90 @@ void flatPrint(int n, double* u);
 
 
 void inv_double_gs(double* a, int n, double* u, double* b){
-	double A[n][n], U[n][n], G[n][n], 
+	double A[n][n], U[n][n], G[n][n], B[n][n], Ut[n][n], proj[n], gproj[n],
 		aVecs[n][n], uVecs[n][n], gVecs[n][n], wVecs[n][n];
-	double temp[n];
 	int i, j;
 
 	matrixExpand(n, a, A);
 
+	// init G to Identity
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			if(i == j)
+				G[i][j] = 1.0;
+			else
+				G[i][j] = 0.0;
+		}
+	}
+
+	// init U to A
+	for(i = 0; i < n; i++){
+		for(j = 0; j < n; j++){
+			U[i][j] = A[i][j];
+		}
+	}
+
 	// aVecs = aT b/c we want to work with the column vectors. 
 	// re-transp both this and uVecs post-GS to get correct matrices.
 	matrixTranspose(n, A, aVecs);
+	matrixTranspose(n, U, uVecs);
+	matrixTranspose(n, G, gVecs);
 
 	for(i = 0; i < n; i++){
+		// single GS
 		for(j = i+1; j < n; j++){
 			// store component to take off in temp
-			vectorProject(n, aVecs[i], aVecs[j], temp);
+			vectorProject(n, aVecs[j], uVecs[i], uVecs[i], proj);
+			vectorProject(n, aVecs[j], uVecs[i], gVecs[i], gproj);
 
 			// subtract off appropriate component
-			vectorSubtract(n, aVecs[j], temp, aVecs[j]);
+			vectorSubtract(n, uVecs[j], proj, uVecs[j]);
+			vectorSubtract(n, gVecs[j], gproj, gVecs[j]);
 		}
-		vectorNormalize(n, aVecs[i], uVecs[i]);
+		vectorNormalize(n, gVecs[i], uVecs[i], gVecs[i]);
+		vectorNormalize(n, uVecs[i], uVecs[i], uVecs[i]);
 	}
 
-	// re-transp to get correct A and U
+	// for(i = n-1; i >= 0; i--){
+	// 	// double GS
+	// 	for(j = i-1; j >= 0; j--){
+	// 		// store component to take off in temp
+	// 		vectorProject(n, uVecs[i], uVecs[j], proj);
+
+	// 		// subtract off appropriate component
+	// 		vectorSubtract(n, uVecs[j], proj, uVecs[j]);
+	// 	}
+	// 	vectorNormalize(n, aVecs[i], aVecs[i] uVecs[i]);
+	// }
+
+	// re-transp to get correct A, G, and U.
 	matrixTranspose(n, aVecs, A);
 	matrixTranspose(n, uVecs, U);
+	matrixTranspose(n, gVecs, G);
 
+	printf("A:\n");
 	matrixPrint(n, A);
-	printf("\n");
+	printf("U:\n");
 	matrixPrint(n, U);
+	printf("G:\n");
+	matrixPrint(n, G);
+
+	// we need U's transpose
+	matrixTranspose(n, U, Ut);
+
+	// Now, find the inverse
+	matrixMultiply(n, G, Ut, B);
+
+	printf("A:\n");
+	matrixPrint(n, A);
+	printf("B:\n");
+	matrixPrint(n, B);
+
+	// check that AB is Identity.
+	matrixMultiply(n, A, B, G);
+	printf("AB: (should be Identity)\n");
+	matrixPrint(n, G);
+
 }
 
 void vectorSubtract(int n, double* u, double* v, double* w){
@@ -66,16 +120,6 @@ void vectorSubtract(int n, double* u, double* v, double* w){
 
 	for(i = 0; i < n; i++){
 		w[i] = u[i] - v[i];
-	}
-
-	return;
-}
-
-void vectorAdd(int n, double* u, double* v, double* w){
-	int i;
-
-	for(i = 0; i < n; i++){
-		w[i] = u[i] + v[i];
 	}
 
 	return;
@@ -105,22 +149,22 @@ void dotProduct(int n, double* u, double* v, double* w){
 	return;
 }
 
-void vectorProject(int n, double* u, double* v, double* w){
+void vectorProject(int n, double* u, double* v, double* x, double* w){
 	double product[1], magnitude[1], c;
 
 	dotProduct(n, u, v, product);
-	vectorMagnitude(n, u, magnitude);
+	vectorMagnitude(n, v, magnitude);
 	c = *product / pow(*magnitude, 2);
-	vectorScale(n, u, c, w);
+	vectorScale(n, x, c, w);
 
 	return;
 }
 
-void vectorNormalize(int n, double* u, double* w){
+void vectorNormalize(int n, double* u, double* v, double* w){
 	int i;
 	double magnitude[1];
 
-	vectorMagnitude(n, u, magnitude);
+	vectorMagnitude(n, v, magnitude);
 
 	for(i = 0; i < n; i++){
 		w[i] = u[i] / (*magnitude);
@@ -237,7 +281,7 @@ void flatPrint(int n, double* u){
 }
 
 int main(){
-	int i, j, sum = 1, n = 3;
+	int i, j, sum = 1, n = 2;
 	double a[n][n], u[n][n], b[n][n], x[n], y[1], 
 		flatA[n*n], flatU[n*n], flatB[n*n];
 
