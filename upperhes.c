5b9279d3f18df184fb54982390a8ceac981a39c1
WillChilds-Klein <willck93@gmail.com>
2014-03-13 14:37:29 -0400

    update .gitignore

--- upperhes.c
+++ upperhes.c
@@ -8,9 +8,8 @@
 #define EPS (.0000001)
 
 void upperhes(int n, double *a, double *u, double *b);
-void kill_ze_offenders(int n, double b[][n], int i, int j, double phi);
+void fast_multiply(int n, double b[][n], int i, int j, double phi);
 void upperhes_slow(int n, double *aFlat, double *uFlat, double *bFlat);
-void assignVals(int n, double left[][n], double right[][n], double phi, int i, int j);
 
 
 // MATRIX ROUTINES: user must allocate memory for retrun param w
@@ -28,12 +27,6 @@ void vectorPrint(int n, double *u);
 void matrixPrint(int n, double u[][n]);
 void flatPrint(int n, double *u);
 
-// v0 - compose sparse u1,u2,...,un and actually multiply them out
-// v1 - all of this multiplication ends up being too slow, so instead
-// of actually composing the full sparse matrices, just apply the pertinent
-// trig functions w vals, and do the same to the identity to iteratively 
-// compose u.
-
 void upperhes(int n, double *aFlat, double *uFlat, double *bFlat){
 	int i, j, k;
 	double a[n][n], u[n][n], b[n][n], left[n][n], right[n][n], 
@@ -49,23 +42,19 @@ void upperhes(int n, double *aFlat, double *uFlat, double *bFlat){
 	identity(n, u);
 	identity(n, uInv);
 
-	// perform ze itrations
+	// perform itrations
 	for(j = 0; j < n; j++){
 		for(i = n-1; i > j+1; i--){
-			// init left and right to I each at start of each iter
-			identity(n, left);
-			identity(n, right);
-
 			// find x and y, then calculate phi
 			x = b[i-1][j];
 			y = b[i][j];
 			phi = atan(-1 * (y / x));
 
 			// kill offenders of b.
-			kill_ze_offenders(n, b, i, j, phi);
+			fast_multiply(n, b, i, j, phi);
 
-			// symmetrically kill "offenders" of u.
-			kill_ze_offenders(n, u, i, j, phi);
+			// kill corresponding "offenders" of u.
+			fast_multiply(n, u, i, j, phi);
 		}
 	}
 
@@ -76,8 +65,37 @@ void upperhes(int n, double *aFlat, double *uFlat, double *bFlat){
 	return;
 }
 
-void kill_ze_offenders(int n, double b[][n], int i, int j, double phi){
+void fast_multiply(int n, double b[][n], int i, int j, double phi){
+	int k;
+	double temp1[2][n], temp2[n][2];
 
+	printf("i: %d, j: %d, b:\n", i, j);
+	matrixPrint(n, b);
+
+	// simulate left multiplication
+	for(k = 0; k < n; k++){
+		temp1[0][k] = cos(phi)*b[i-1][k] + sin(phi)*b[i][k];
+		temp1[1][k] = -1*sin(phi)*b[i-1][k] + cos(phi)*b[i][k];
+	}
+
+	memcpy(b[i-1], temp1[0], n*sizeof(double));
+	memcpy(b[i], temp1[1], n*sizeof(double));
+
+	// simulate right multiplication
+	for(k = 0; k < n; k++){
+		temp2[k][0] = cos(phi)*b[k][i-1] + sin(phi)*b[k][i];
+		temp2[k][1] = -1*sin(phi)*b[k][i-1] + cos(phi)*b[k][i];
+	}
+
+	for(k = 0; k < n; k++){
+		b[k][i-1] = temp2[k][0];
+		b[k][i] = temp2[k][1];
+	}
+
+	printf("b:\n");
+	matrixPrint(n, b);
+
+	return;
 }
 
 void upperhes_slow(int n, double *aFlat, double *uFlat, double *bFlat){
@@ -95,20 +113,29 @@ void upperhes_slow(int n, double *aFlat, double *uFlat, double *bFlat){
 	identity(n, u);
 	identity(n, uInv);
 
-	// perform ze itrations
+	// perform iterations
 	for(j = 0; j < n; j++){
 		for(i = n-1; i > j+1; i--){
 			// init left and right to I each at start of each iter
 			identity(n, left);
 			identity(n, right);
 
+			// assign rotation values to left matrix
+			left[i-1][i-1] = cos(phi);
+			left[i-1][i] = -1*sin(phi);
+			left[i][i-1] = sin(phi);
+			left[i][i] = cos(phi);
+
+			// assign rotation values to right matrix
+			right[i-1][i-1] = cos(phi);
+			right[i-1][i] = sin(phi);
+			right[i][i-1] = -1*sin(phi);
+			right[i][i] = cos(phi);
+
 			// find x and y, then calculate phi
 			x = b[i-1][j];
 			y = b[i][j];
 			phi = atan(-1 * (y / x));
-
-			// assign values to sparse matrices appropriately
-			assignVals(n, left, right, phi, i, j);
 
 			// keel ze y
 			matrixMultiply(n, left, b, b);
@@ -123,20 +150,6 @@ void upperhes_slow(int n, double *aFlat, double *uFlat, double *bFlat){
 	// write to outputs
 	matrixFlatten(n, u, uFlat);
 	matrixFlatten(n, b, bFlat);
-
-	return;
-}
-
-void assignVals(int n, double left[][n], double right[][n], double phi, int i, int j){
-	left[i-1][i-1] = cos(phi);
-	left[i-1][i] = -1*sin(phi);
-	left[i][i-1] = sin(phi);
-	left[i][i] = cos(phi);
-
-	right[i-1][i-1] = cos(phi);
-	right[i-1][i] = sin(phi);
-	right[i][i-1] = -1*sin(phi);
-	right[i][i] = cos(phi);
 
 	return;
 }
@@ -272,8 +285,9 @@ void flatPrint(int n, double* u){
 	return;
 }
 
+/**/
 int main(){
-	int i, j, sum = 0, n = 8;
+	int i, j, sum = 0, n = 4;
 	double a[n][n], u[n][n], b[n][n],
 		aFlat[n*n], uFlat[n*n], bFlat[n*n],
 		uInv[n][n], test[n][n];
@@ -285,7 +299,7 @@ int main(){
 		}
 	}
 
-	// flatten input, run upperhes, then expand outputs
+	// flatten input, run upperhes_slow, then expand outputs
 	matrixFlatten(n, a, aFlat);
 	upperhes_slow(n, aFlat, uFlat, bFlat);
 	matrixExpand(n, uFlat, u);
@@ -298,8 +312,25 @@ int main(){
 	matrixPrint(n, u);
 	printf("b:\n");
 	matrixPrint(n, b);
+
+	// flatten input, run upperhes, then expand outputs
+	matrixFlatten(n, a, aFlat);
+	upperhes(n, aFlat, uFlat, bFlat);
+	matrixExpand(n, uFlat, u);
+	matrixExpand(n, bFlat, b);
+
+	// check outputs.
+	printf("a:\n");
+	matrixPrint(n, a);
+	printf("u:\n");
+	matrixPrint(n, u);
+	printf("b:\n");
+	matrixPrint(n, b);
+
+	// test code
+	/** /
 	printf("test code:\n\n");
-	printf("u uT (should be identity)\n");
+	printf("u uT (should be identity, confirms orthogonality)\n");
 	matrixTranspose(n, u, uInv);
 	matrixMultiply(n, u, uInv, test);
 	matrixPrint(n, test);
@@ -309,16 +340,15 @@ int main(){
 	matrixPrint(n, test);
 	printf("original a:\n");
 	matrixPrint(n, a);
-
 	printf("does it work?\n");
 	for(i = 0; i < n; ++i){
 		for(j = 0; j < n; j++){
 			// approximate equals
 			if(fabs(test[i][j] - a[i][j]) > EPS){
 				printf("NO\n");
-				printf("a[%d][%d]: %f, test[%d][%d]: %f\n", i, j, a[i][j], 
+				printf("a[%d][%d]: %f =/= test[%d][%d]: %f\n", i, j, a[i][j], 
 					i, j, test[i][j]);
-				i = INT_MAX;
+				i = INT_MAX; // use i as inequality flag
 				break;
 			}
 		}
@@ -326,8 +356,10 @@ int main(){
 			break;
 		}
 	}
-	if(i < INT_MAX)
+	if(i < INT_MAX) // test flag.
 		printf("YES\n");
+	/**/
 
 	return 1;
 }
+/**/
